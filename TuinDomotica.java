7992eff4ed08a722bafe8b4e1b5c7598788f2f64
QazCetelic <qaz.cetelic@protonmail.com>
2022-04-20 15:57:19 +0200

    Fix type in `setSlimmeSchakelaar`

--- TuinDomotica.java
+++ TuinDomotica.java
@@ -23,7 +23,7 @@ public class TuinDomotica {
         return slimmeschakelaar;
     }
 
-    public void setSlimmeschakelaar(schakelaar slimmeschakelaar) {
+    public void setSlimmeschakelaar(Schakelaar slimmeschakelaar) {
         this.slimmeschakelaar = slimmeschakelaar;
     }
 }
